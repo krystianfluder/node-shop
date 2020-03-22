8550e7b5151ef8ab75449c8f7d2cafe2751ec8a7
Krystian Fluder <krystian.fluder@outlook.com>
2020-03-22 11:03:15 +0100

    Add user middleware

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
+
 // const { mongoConnect } = require("./util/database2");
 
 const app = express();
@@ -20,13 +23,42 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// sequelize.sync();
-app.listen(8080);
-// mongoConnect(() => {
-//   app.listen(3000);
-// });
+Product.belongsTo(User, {
+  constraints: true,
+  onDelete: "CASCADE"
+});
+
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then(() => {
+    return User.findByPk(1);
+    // app.listen(3000);
+  })
+  .then(user => {
+    if (!user) {
+      User.create({ name: "test", email: "test@test.com" });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    app.listen(8080);
+  })
+  .catch(err => {
+    console.log(err);
+  });
